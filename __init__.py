a3f087fd2d1ee9fdda015e0dc54a9a44a65b4086
Darek Maciąg <darekmaciag@wp.pl>
2021-02-05 02:46:45 +0100

    installations

---
+++ __init__.py
@@ -0,0 +1,23 @@
+import os
+import logging
+
+from flask import Flask, render_template
+
+from app import database
+from app.dash_setup import register_dashapps
+
+
+def create_app():
+    app = Flask(__name__)
+    app.config['SECRET_KEY'] = os.getenv('SECRET_KEY')
+    logging.basicConfig(level=logging.DEBUG)
+
+    @app.route('/')
+    def home():
+        return render_template('index.html')
+
+    database.init_app(app)
+
+    register_dashapps(app)
+
+    return app
